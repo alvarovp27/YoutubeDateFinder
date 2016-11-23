91c0ff73b6dc588136206d80cb19a60868c63544
Alvaro Valencia <avalenciaparra@gmail.com>
2016-11-23 01:25:44 +0100

    feat: first working attempt for logging in to google account

--- api.js
+++ api.js
@@ -1,13 +1,50 @@
 var express = require('express');
 var status = require('http-status');
+var google = require("googleapis");
+var secrets = require('./secrets.json');
 
 var router = express.Router();
 
-router.get('/helloWorld', function(req, res){
-    res.json({
-        helloWorld: "It worked!"
-    });
-    console.log("It worked!")
+var OAuth2 = google.auth.OAuth2;
+
+var oauth2Client = new OAuth2(secrets.web.client_id, secrets.web.client_secret, "http://localhost:5000");
+
+// generate a url that asks permissions for Google+ and Google Calendar scopes
+var scopes = [
+  'https://www.googleapis.com/auth/youtube'
+];
+
+var url = oauth2Client.generateAuthUrl({
+  access_type: 'online', // 'online' (default) or 'offline' (gets refresh_token)
+  scope: scopes // If you only need one scope you can pass it as string
+});
+
+router.get('/signIn', function(req, res){
+    res.redirect(url)
+    
+});
+
+router.get("/tokens", function(req, res) {
+
+  var code = req.query.code;
+
+  console.log(code);
+
+  oauth2Client.getToken(code, function(err, tokens) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+      return;
+    }
+
+    console.log("allright!!!!");
+
+    console.log(err);
+    console.log(tokens);
+    oauth2Client.setCredentials(tokens);
+
+    res.send(tokens);
+  });
 });
 
 module.exports = router;
