00a6389df44211c5b1c42144a1e041f457b9821e
Alvaro Valencia <avalenciaparra@gmail.com>
2016-11-23 22:58:06 +0100

    refactor: I've finalized the login procedure by refactoring the code, making it easier to call it from other parts of the code. Also, I've created a method for authorizating the use of the Google API by passing the tokens kept in the session as parameter

--- auth.js
+++ auth.js
@@ -3,65 +3,90 @@ var google = require("googleapis");
 var secrets = require('./secrets.json');
 var session = require("express-session");
 
-function setupAuth(app){
-    app.use(session({secret: 'this is a secret'}));
 
+function getOAuth2Client(){
     var OAuth2 = google.auth.OAuth2;
-
     var oauth2Client = 
-    new OAuth2(secrets.web.client_id, secrets.web.client_secret, secrets.web.redirect_uris[0]);
+        new OAuth2(secrets.web.client_id, secrets.web.client_secret, secrets.web.redirect_uris[0]);
 
+    return oauth2Client;
+}
+
+function getAuthenticatingUrl(){
+    var oauth2Client = getOAuth2Client();
     var scopes = [
-        'https://www.googleapis.com/auth/youtube'
+        'https://www.googleapis.com/auth/youtube.readonly'
     ];
 
+    //That's for generating the login url
     var url = oauth2Client.generateAuthUrl({
         access_type: 'offline', 
         scope: scopes
     });
 
-    app.get('/signIn', function(req, res){
-        res.redirect(url)
+    return url;
+}
+
+//We've to call to this method in order to authorize the petitions to
+//the Google's API. We just have to passs the tokens from the session as
+//parameter.
+function getOAuth2ClientAuthenticated(tokens){
+    var oauth2Client = getOAuth2Client();
+    oauth2Client.setCredentials(tokens);
+    return oauth2Client;
+}
+
+//Middleware function
+function setupAuth(app){
+    app.use(session({secret: 'xFcSksjlaowNgEsXgGHjhB&57624kjBgf390dvJHaZq'}));
+
+    //Creates the oauth2Client object
+    var oauth2Client = getOAuth2Client();
+
+    //Sign in button
+    app.get('/login', function(req, res){
+        if(!req.session.channelName){
+            res.redirect(getAuthenticatingUrl());
+        } else {
+            res.send({error: "You're already logged in."})
+        }
     });
 
+    //Callback procedure
     app.get("/callback", function(req, res) {
         var code = req.query.code;
-
-        console.log(code);
-
+        //Let's take the tokens
         oauth2Client.getToken(code, function(err, tokens) {
             if (err) {
                 console.log(err);
                 res.send(err);
                 return;
             }
-            
-            oauth2Client.setCredentials(tokens);
+            //Now we're to take the name of the user's channel.
+            //First of all, we create the youtube variable which will let us do petitions
             var youtube = google.youtube({
                 version: 'v3',
-                auth: oauth2Client
+                auth: getOAuth2ClientAuthenticated(tokens)
             });
-            
+            //Now we do the petition to the google's API
             youtube.channels.list({
                 part: 'snippet',
                 mine: true
             }, function(err, data){
-                console.log(err);
-                console.log('datossss: '+data);
                 if(!err){
-                    console.log(data.items[0].snippet.title);
-                    //does not work
                     req.session.channelName = data.items[0].snippet.title;
-                    //console.log("showing req var "+hola);
+                    console.log('channel name!!: '+req.session.channelName);
+                    req.session.tokens = tokens;
+                    res.redirect('/');
+                } else {
+                    console.log(err);
+                    req.send({error: 'an error happened when authenticating', trace: err});
                 }
-                console.log('channel name!!: '+req.session.channelName);
-                req.session.token = tokens;
-                res.redirect('/');
             });
-
         });
     });
 
+    //the logount endpoint will destroy the current user's session
     app.get('/logout', function(req, res) {
         req.session.destroy(function(err){
             if(err){
