14f666c39813516d602a7814013437751cd6ed76
Alvaro Valencia <avalenciaparra@gmail.com>
2016-11-24 19:44:22 +0100

    feat: added an endpoint for searching for videos by date

--- api.js
+++ api.js
@@ -3,6 +3,7 @@ var status = require('http-status');
 var session = require("express-session");
 var google = require('googleapis');
 var auth = require('./auth');
+var bodyparser = require('body-parser');
 
 var router = express.Router();
 router.use(session({secret: 'this is a secret'}));
@@ -12,6 +13,46 @@ router.get("/test", function(req, res){
   console.log(req.session.tokenn);
   console.log(req.session.channelName);
   res.send(req.session.channelName);
+});
+
+router.get("/search", function(req, res){
+
+  if(!req.session.tokens){
+    res.send({error: "You have to be authenticated."});
+  } else {
+    var query = req.query.query;
+    var publishedAfter = req.query.publishedAfter;
+    var publishedBefore = req.query.publishedBefore;
+    var nextPageToken = req.query.nextPageToken;
+
+    var queryJson = {
+      part: 'id,snippet',
+      q: query,
+      maxResults: '50',
+      order: 'date',
+      publishedAfter: publishedAfter,
+      publishedBefore: publishedBefore,
+      type: 'video'
+    }
+
+    if(nextPageToken){
+      queryJson['pageToken'] = nextPageToken;
+      console.log(queryJson)
+    }
+
+    var youtube = auth.youtube(req.session.tokens);
+    youtube.search.list(queryJson, function (err, data) {
+      if (err) {
+        console.error('Error: ' + err);
+      }
+      if (data) {   
+        console.log("number of videos retrieved: "+data.items.length)
+        var formatedData = buildVideoResponse(data);
+        console.log(formatedData.videos.length)
+        res.send(formatedData);
+      }
+    });
+  }
 });
 
 router.get("/testSearchVideo", function(req, res){
@@ -39,28 +80,32 @@ router.get("/testSearchVideo", function(req, res){
     if (err) {
       console.error('Error: ' + err);
     }
-    if (data) {
-      var nextPageToken = data.nextPageToken;
-      var totalResults = data.pageInfo.totalResults;
-      var videos = [];
-
-      data.items.forEach(function(value){
-        var video = {
-                  title: value.snippet.title,
-                  thumbnails: value.snippet.thumbnails.url,
-                  channel: value.snippet.channelTitle,
-                  description: value.snippet.description,
-                  videoId: value.id.videoId,
-                  publishedAt: value.snippet.publishedAt
-                 };
-
-        videos.push(video);
-      });
-      
-      res.send({nextPageToken: nextPageToken, totalResults: totalResults, videos: videos});
+    if (data) {   
+      res.send(buildVideoResponse(data));
     }
   });
 
 });
+
+function buildVideoResponse(data){
+  var nextPageToken = data.nextPageToken;
+  var totalResults = data.pageInfo.totalResults;
+  var videos = [];
+
+  data.items.forEach(function(value){
+    var video = {
+              title: value.snippet.title,
+              thumbnails: value.snippet.thumbnails.url,
+              channel: value.snippet.channelTitle,
+              description: value.snippet.description,
+              videoId: value.id.videoId,
+              publishedAt: value.snippet.publishedAt
+              };
+
+    videos.push(video);
+  });
+
+  return {nextPageToken: nextPageToken, totalResults: totalResults, videos: videos};
+}
 
 module.exports = router;
